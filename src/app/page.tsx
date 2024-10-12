a5b9652c98d2fea7a7c3d40d771572ce4c71b630
Chis Nghiax <atuanvptd@gmail.com>
2024-10-12 17:51:06 +0700

    update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ function Hero() {
         />
         <div className="pb-24 pt-16 sm:pb-32 sm:pt-24 md:pb-48 md:pt-32">
           <h1 className="font-display text-balance text-6xl/[0.9] font-medium tracking-tight text-gray-950 sm:text-8xl/[0.8] md:text-9xl/[0.8]">
-            Ncmaz FSE.
+            Ncmaz Fse Theme.
           </h1>
           <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">
             Ncmaz Fse - Blog, News and Magazine Full Site Editing WordPress
